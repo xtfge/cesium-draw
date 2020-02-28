d14d98116dc5230858f459947736d63d943919f0
xtfge <874406328@qq.com>
2020-02-28 21:17:18 +0800

    优化配置

--- src/index.js
+++ src/index.js
@@ -2,20 +2,16 @@
  * @Author: zhangbo
  * @E-mail: zhangb@geovis.com.cn
  * @Date: 2019-10-10 15:55:20
- * @LastEditors  : zhangbo
- * @LastEditTime : 2019-12-26 11:52:35
+ * @LastEditors: zhangbo
+ * @LastEditTime: 2020-02-28 14:24:50
  * @Desc: 
  */
 import Vue from 'vue'
 import './plugins/element.js'
-import $ from 'jquery'
-import utils from '@/js/utils'
-import 'jstree'
-import 'jstree/dist/themes/default/style.min.css'
-import "@/assets/css/iconfont.css";
 import drawViewer from './components/cesiumDrawViewer'
+import "@/assets/css/iconfont.css"
+
 Vue.config.productionTip = false
-window.$ = $
 
 drawViewer.install = Vue => {
   Vue.component(drawViewer.name, drawViewer)
