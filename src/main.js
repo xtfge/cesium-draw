406521b4c9de3a8183a56826847d40cafaa4c105
xtfge <874406328@qq.com>
2020-02-28 21:19:02 +0800

    remove jstree

--- src/main.js
+++ src/main.js
@@ -2,26 +2,16 @@
  * @Author: zhangbo
  * @E-mail: zhangb@geovis.com.cn
  * @Date: 2019-10-09 19:42:40
- * @LastEditors  : zhangbo
- * @LastEditTime : 2019-12-26 19:13:48
+ * @LastEditors: zhangbo
+ * @LastEditTime: 2020-02-28 13:50:56
  * @Desc: 
  */
 import Vue from 'vue'
 import './plugins/element.js'
 import App from './App'
-import router from './router'
-import Router from 'vue-router'
-import 'jstree'
-import 'jstree/dist/themes/default/style.min.css'
-import $ from 'jquery'
 import "@/assets/css/iconfont.css";
-// Vue.use(cesiumDrawHandler)
-// Vue.use(cesiumDrawHandler)
 
 Vue.config.productionTip = false
-Vue.use(Router)
-window.$=$
 new Vue({
-  render: h => h(App),
-  router,
+  render: h => h(App)
 }).$mount('#app')
